08e21d47952586103918ac6389e8d756ff4d8ab9
wolfria <wolfria0606@gmail.com>
2017-02-06 22:22:49 +0900

    色々したよ。

--- www/js/memo.js
+++ www/js/memo.js
@@ -4,12 +4,11 @@ var ncmb = new NCMB("c578c67b1aa7a69c072b8f090e29e7e64ce17ebb7383d54bc3dd2c26c14
 // メモを保存する。
 function imputMemo() {
     // inputの値を取得
-    var memo = $('[id=output1]').val();
+    var seed = $('#output1').val();
     // 取得した値が空だったら
-    if (memo == null || memo == "") {
+    if (seed == null || seed == "") {
         // キャンセルアラートを表示
         $("#amozan").html("何か書いてね");
-        // 何か書き込まれていたら
     } else {
         // メモを保存
         saveMemo(memo);
@@ -19,9 +18,9 @@ function imputMemo() {
 // mBaaSへデータの保存
 function saveMemo(memo) {
     // 保存先クラスを作成
-    var MemoData = ncmb.DataStore("MemoData");
+    var memoData = ncmb.DataStore("MemoData");
     // MemoDataの保存総数を取得
-    MemoData.count()
+    memoData.count()
         // MemoDataを検索
         .fetchAll()
         // 検索に成功した場合の処理
@@ -37,10 +36,8 @@ function saveMemo(memo) {
                 .then(function() {
                     // pタグに書き込まれた内容を表示
                     $("#amozan").text(memo + " を保存したよ。");
-                    $("#amozan").fadeOut(3000);
                     // inputの値を空にする
-                    $("[id=output1]").val("");
-                    setTimeout();
+                    $("#output1").val("");
                 })
                 .catch(function(err) {
                     // 保存に失敗した場合の処理
@@ -68,7 +65,7 @@ ons.ready(function() {
                 //取得したデータの数だけ回す
                 for (i = 0; i < results.length; i++) {
                     //リストの末端に追加
-                    $('ons-list').append('<ons-list-item id="oyayoso' + [i] + '" class="oyayoso" modifier="longdivider">' + results[i].memo + '<div class="right"><ons-switch id="oyaswitch' + [i] + '" class="oyayoso oyayososwitch"></ons-switch></div></ons-list-item>');
+                    $('ons-list').append('<ons-list-item id="oya' + [i] + '" modifier="longdivider">' + results[i].memo + '<div class="right"><ons-switch class="oyaswitch"></ons-switch></div></ons-list-item>');
                 }
             })
             .catch(function(error) {
@@ -78,35 +75,41 @@ ons.ready(function() {
     })
 });
 
-$(document).on('change', '.oyayososwitch', function(event) {
+$(document).on('change', '.oyaswitch', function(event) {
     // clickイベントで発動する処理
     var oya = $(this).closest("ons-list-item");
-    var id = $(oya).attr("id");
+    var oyaid = $(oya).attr("id");
+    var idnum = oyaid.slice(3);
     var memoname = $(oya).text();
     var memoname = memoname.substring(0, memoname.indexOf(" "));
+    console.log(oyaid);
     if (this.checked == true) {
         $.ajax({
             type: 'GET',
             url: 'https://api.apitore.com/api/8/word2vec-neologd-jawiki/distance?access_token=b8be493b-a34a-42d6-a5e8-7f606e3d13ee&word=' + memoname + '&num=8'
+            //url: 'https://api.apitore.com/api/8/word2vec-neologd-jawiki/distance?access_token=96b1c2aa-eac1-4692-a0ad-e29d8fddd9a9&word=' + memoname + '&num=8'
         }).success(function(data) {
             var item = "";
             for (var i = 0, l = 8; i < l; i++) {
                 var word = data.distances[i].word;
-                var item = item + '<ons-list-item id="koyoso'+[i]+'" class="koyoso '+ id +'">' + ' ・ ' + word + '<div class="right"><ons-switch class="koyoso koyososwitch '+ id +'"></ons-switch></div></ons-list-item>';
+                var item = item + '<ons-list-item id="ko' + idnum + '_' + [i] + '" class="' + oyaid + ' ko"> ・ ' + word + '<div class="right"><ons-switch class="' + oyaid + ' koswitch"></ons-switch></div></ons-list-item>';
             }
-            $("#" + id).after(item);
+            $("#" + oyaid).after(item);
         }).error(function(data) {
             alert('error!!!');
         });
     } else {
-        $("."+id).hide();
+        $("." + oyaid).remove();
     }
 });
 
-$(document).on('change', '.koyososwitch', function(event) {
+$(document).on('change', '.koswitch', function(event) {
     // clickイベントで発動する処理
     var ko = $(this).closest("ons-list-item");
-    var id = $(ko).attr("id");
+    var koid = $(ko).attr("id");
+    var koclass = $(ko).attr("class");
+    var koclass = koclass.substring(0, koclass.indexOf(" "));
+    var idnum = koclass.slice(2);
     var memoname = $(ko).text();
     var memoname = memoname.slice(3);
     var memoname = memoname.substring(0, memoname.indexOf(" "));
@@ -114,36 +117,87 @@ $(document).on('change', '.koyososwitch', function(event) {
         $.ajax({
             type: 'GET',
             url: 'https://api.apitore.com/api/8/word2vec-neologd-jawiki/distance?access_token=b8be493b-a34a-42d6-a5e8-7f606e3d13ee&word=' + memoname + '&num=8'
+            //url: 'https://api.apitore.com/api/8/word2vec-neologd-jawiki/distance?access_token=96b1c2aa-eac1-4692-a0ad-e29d8fddd9a9&word=' + memoname + '&num=8'
         }).success(function(data) {
             var item = "";
             for (var i = 0, l = 8; i < l; i++) {
                 var word = data.distances[i].word;
-                var item = item + '<ons-list-item class="magoyoso '+ id +'" modifier="nodivider" tappable>' + ' ・・ ' + word + '</ons-list-item>';
+                var item = item + '<ons-list-item id=mago' + idnum + '_' + [i] + ' class="' + koid + '_' + idnum + ' ' + koclass + ' mago" modifier="nodivider" tappable>' + ' ・・ ' + word + '</ons-list-item>';
             }
-            $("#" + id).after(item);
+            $("#" + koid).after(item);
         }).error(function(data) {
             alert('error!!!');
         });
-    }else{
-      $("."+id).hide();
+    } else {
+        $("." + koid + "_" + idnum).remove();
     }
 });
 
 // ランダムに保存したデータを表示する
 function randamsetData() {
-    //
-    var MemoData = ncmb.DataStore("MemoData");
-    MemoData.fetchAll()
-        .then(function(results) {
-            memonano1 = results[Math.floor(Math.random() * results.length)].memo;
-            memonano2 = results[Math.floor(Math.random() * results.length)].memo;
-            while (memonano1 == memonano2) {
+    var word = $('#word1').val();
+    var idea = $('#output2').val();
+    if (word == null || word == "") {
+        var MemoData = ncmb.DataStore("MemoData");
+        MemoData.fetchAll()
+            .then(function(results) {
+                memonano1 = results[Math.floor(Math.random() * results.length)].memo;
                 memonano2 = results[Math.floor(Math.random() * results.length)].memo;
-            }
-            $('#word1').val(memonano1);
-            $('#word2').val(memonano2);
-        })
-        .catch(function(err) {
-            console.log("Not Find" + err);
-        });
+                while (memonano1 == memonano2) {
+                    memonano2 = results[Math.floor(Math.random() * results.length)].memo;
+                }
+                $('#word1').val(memonano1);
+                $('#word2').val(memonano2);
+                $('#randambutton').text("保存する");
+                $('#randambutton').after('<ons-button id="randambutton2" modifier="large" onclick="randamsetData2()">もう一度生み出す</ons-button>');
+            })
+            .catch(function(err) {
+                console.log("Not Find" + err);
+            });
+    } else if (idea.length >= 1){
+        // 保存先クラスを作成
+        var MemoData = ncmb.DataStore("MemoData");
+        MemoData.fetchAll()
+            // 検索に成功した場合の処理
+            .then(function(results) {
+                // クラスインスタンスを生成
+                var memoData = new MemoData();
+                // 値を設定
+                memoData.set("memo",idea);
+                // 保存を実施
+                memoData.save()
+                    .then(function() {
+                        // inputの値を空にする
+                        $('#word1').val("");
+                        $('#word2').val("");
+                        $("#output2").val("");
+                    })
+                    .catch(function(err) {
+                        // 保存に失敗した場合の処理
+                        console.log("Not Save ! error:" + err);
+                    })
+            })
+            .catch(function(err) {
+                // 検索に失敗した場合の処理
+            });
+    } else {
+      $("#output2").attr("placeholder","");
+    }
+}
+
+function randamsetData2(){
+  var MemoData = ncmb.DataStore("MemoData");
+  MemoData.fetchAll()
+      .then(function(results) {
+          memonano1 = results[Math.floor(Math.random() * results.length)].memo;
+          memonano2 = results[Math.floor(Math.random() * results.length)].memo;
+          while (memonano1 == memonano2) {
+              memonano2 = results[Math.floor(Math.random() * results.length)].memo;
+          }
+          $('#word1').val(memonano1);
+          $('#word2').val(memonano2);
+      })
+      .catch(function(err) {
+          console.log("Not Find" + err);
+      });
 }
